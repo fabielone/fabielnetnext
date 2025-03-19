b391ac5ed639bec37af26bbc5e6d9cc0768ffb04
Fabielone <64607173+fabielone@users.noreply.github.com>
2025-03-19 02:10:45 +0000

    changes to chatwoot

--- app/components/utils/chatwoot.js
+++ app/components/utils/chatwoot.js
@@ -1,19 +1,34 @@
-'use client'
+'use client'; // Mark this component as a Client Component
+
 import Script from 'next/script';
 
 const ChatwootScript = () => {
   return (
-    <Script
-      id="chatwoot-sdk"
-      strategy="lazyOnload" // Load the script after the page becomes interactive
-      src="https://app.chatwoot.com/packs/js/sdk.js"
-      onLoad={() => {
-        window.chatwootSDK.run({
-          websiteToken: 'wVPtYLy3b9Bn3f3AAvAPrVB5',
-          baseUrl: 'https://app.chatwoot.com',
-        });
-      }}
-    />
+    <>
+      {/* Set Chatwoot settings globally */}
+      <Script id="chatwoot-settings" strategy="beforeInteractive">
+        {`
+          window.chatwootSettings = {
+            position: "right",
+            type: "expanded_bubble",
+            launcherTitle: "Chat with us"
+          };
+        `}
+      </Script>
+
+      {/* Load Chatwoot SDK */}
+      <Script
+        id="chatwoot-sdk"
+        strategy="lazyOnload" // Load after the page becomes interactive
+        src="https://app.chatwoot.com/packs/js/sdk.js"
+        onLoad={() => {
+          window.chatwootSDK.run({
+            websiteToken: 'wVPtYLy3b9Bn3f3AAvAPrVB5',
+            baseUrl: 'https://app.chatwoot.com',
+          });
+        }}
+      />
+    </>
   );
 };
 
